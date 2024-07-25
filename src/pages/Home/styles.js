0c61d3c9d465d4193cd72d02d7db38578453b9c8
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-07-24 23:12:53 -0300

    Adjust Home Page

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,12 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     height: 100vh;
+    overflow: hidden;
 `;
 
 export const Content = styled.div`
     padding: 0 106px 60px 123px;
-    height: calc(100vh - 292px);
-    overflow-y: auto;
 `;
 
 export const Section = styled.div`
@@ -16,16 +15,20 @@ export const Section = styled.div`
     justify-content: space-between;
     
     &[title="Menu"] {
-        padding: 50px 106px 37.5px 123px;
+        padding: 30px 106px 37.5px 123px;
     }
     
     &[title="Meus Filmes"] {
         display: flex;
         flex-direction: column;
         gap: 24px;
-    }
-    `;
 
+        height: calc(100vh - 320px);
+        overflow-y: auto;
+
+        scrollbar-color: ${({ theme }) => theme.COLORS.ROSE} transparent;
+    }
+`;
 
 export const Menu = styled.div`
     display: flex;
