53cf5f721840f858753ec66bdbfef696fc098040
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-09-10 10:38:24 -0300

    Add: Create Login funcionality

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FiMail, FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
@@ -9,9 +10,14 @@ import { Button } from "../../components/Button"
 import { Input } from "../../components/Input"
 
 export function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const data = useAuth();
-    console.log("MEU CONTEXTO =>", data);
+    const { signIn } = useAuth();
+
+    function handleSignIn() {
+        signIn({ email, password });
+    }
 
     return (
         <Container>
@@ -26,15 +32,20 @@ export function SignIn() {
                     type="text"
                     icon={FiMail}
                     margin={"0px 0px 8px 0px"}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Entrar" />
+                <Button
+                    title="Entrar"
+                    onClick={handleSignIn}
+                />
 
                 <Link to="/register">
                     Criar conta
