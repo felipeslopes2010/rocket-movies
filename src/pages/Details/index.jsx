4a32d87ef14e56e6f4b7eb36da2ca13364789c56
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-09-22 13:57:34 -0300

    Adjust: Formate Date in Details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -51,6 +51,20 @@ export function Details() {
         return stars;
     }
 
+    function formateDate(dateCreatedAt) {
+
+    const date = new Date(data.created_at);
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
+
     useEffect(() => {
         async function fetchMovie() {
             const response = await api.get(`/movies/${params.id}`);
@@ -97,7 +111,7 @@ export function Details() {
                             <img src={`tmp/uploads/${user.avatar}`} alt={`Foto de ${user.name}`} />
                             <span>{`Por ${user.name}`}</span>
                             <FiClock />
-                            <span>{data.created_at}</span>
+                            <span>{formateDate(data.created_at)}</span>
                         </div>
 
                         {
